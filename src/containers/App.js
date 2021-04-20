6897e29285bbef6c9da3f6d0b208d274fbc765a8
Mishin Yura <mishin.inbox@gmail.com>
2021-04-20 11:32:20 +0300

    Added search box. Closes #7

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import CardList from '../components/CardList';
 import {robots} from "../robots";
+import SearchBox from "../components/SearchBox";
 
 const App = () => {
     return (
         <div className='tc'>
             <h1 className='f1'>RoboFriends</h1>
+            <SearchBox/>
             <CardList robots={robots}/>
         </div>
     )
