84fddf42e637357173b4e09dd40232140a4499d8
Mishin Yura <mishin.inbox@gmail.com>
2021-04-20 11:18:10 +0300

    Added card list. Closes #5

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons'
-import Card from "./components/Card";
+import CardList from "./components/CardList";
 import {robots} from "./robots";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Card
-            id={robots[0].id}
-            name={robots[0].name}
-            email={robots[0].email}
-        />
+        <CardList robots={robots}/>
     </React.StrictMode>,
     document.getElementById('root')
 );
