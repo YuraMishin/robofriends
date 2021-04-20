bc0c2bb4dde3f8ef08c8e513c41494cd9a1d37cc
Mishin Yura <mishin.inbox@gmail.com>
2021-04-20 18:10:48 +0300

    Added scrollable feature to list. Closes #10

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import CardList from '../components/CardList';
 import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
 
 class App extends Component {
     constructor() {
@@ -36,7 +37,9 @@ class App extends Component {
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
-                    <CardList robots={filteredRobots}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
                 </div>
             );
     }
