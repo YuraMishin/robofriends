4c0af7842a26fff75274e01e496bffa9994ec54b
Mishin Yura <mishin.inbox@gmail.com>
2021-04-20 12:27:41 +0300

    Implemented data loading from API Jsonplaceholder. Closes #9

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,23 @@
 import React, {Component} from 'react';
 import './App.css';
 import CardList from '../components/CardList';
-import {robots} from "../robots";
 import SearchBox from "../components/SearchBox";
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => {
+                this.setState({robots: users})
+            });
     }
 
     onSearchChange = (event) => {
@@ -23,15 +30,15 @@ class App extends Component {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
 
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox
-                    searchChange={this.onSearchChange}
-                />
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+        return !robots.length ?
+            <h1>Loading</h1> :
+            (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
     }
 }
 
