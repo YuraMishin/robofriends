c77321610e6760b463af691949961f3e61c2668e
Mishin Yura <mishin.inbox@gmail.com>
2021-04-20 12:05:32 +0300

    Implemented searching feature. Closes #8

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import CardList from '../components/CardList';
 import {robots} from "../robots";
 import SearchBox from "../components/SearchBox";
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1 className='f1'>RoboFriends</h1>
-            <SearchBox/>
-            <CardList robots={robots}/>
-        </div>
-    )
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
+
+    render() {
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+
+        return (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox
+                    searchChange={this.onSearchChange}
+                />
+                <CardList robots={filteredRobots}/>
+            </div>
+        );
+    }
 }
 
 export default App;
