a7bcc4209dc367ba6e2051c0a2c0f902aa351078
Mishin Yura <mishin.inbox@gmail.com>
2021-04-20 10:48:13 +0300

    Added loading robots from robots storage. Closes #4

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons'
 import Card from "./components/Card";
+import {robots} from "./robots";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Card/>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Card
+            id={robots[0].id}
+            name={robots[0].name}
+            email={robots[0].email}
+        />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
